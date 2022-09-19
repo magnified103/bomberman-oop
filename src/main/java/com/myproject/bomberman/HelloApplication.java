8014b3ff93012931218a881fa7bb6f348bcbe626
Haidan1011 <101380985+Haidan1011@users.noreply.github.com>
2022-09-19 22:35:02 +0700

    Add assets file and test some fxgl function

--- src/main/java/com/myproject/bomberman/HelloApplication.java
+++ src/main/java/com/myproject/bomberman/HelloApplication.java
@@ -3,25 +3,60 @@ package com.myproject.bomberman;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.*;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class HelloApplication extends GameApplication {
+    Entity player;
+
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(800);
         settings.setHeight(600);
-        settings.setTitle("BasicGameSample");
+        settings.setTitle("Bomberman");
+        settings.setDeveloperMenuEnabled(true);
     }
 
     @Override
     protected void initGame() {
-        FXGL.entityBuilder()
+        player = FXGL.entityBuilder()
                 .at(150, 150)
                 .view(new Rectangle(40, 40, Color.BLUE))
                 .buildAndAttach();
     }
+    @Override
+    protected void initInput() {
+        Input input = FXGL.getInput();
 
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5);
+            }
+        }, KeyCode.D);
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5);
+            }
+        }, KeyCode.A);
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5);
+            }
+        }, KeyCode.W);
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5);
+            }
+        }, KeyCode.S);
+    }
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module com.myproject.bomberman {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires com.almasb.fxgl.all;
 
     opens com.myproject.bomberman to javafx.fxml;
     exports com.myproject.bomberman;
+
+    opens assets.textures;
 }
