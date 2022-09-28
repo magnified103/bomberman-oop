ba615ad792446af99dfc47c72b651fb69eac45e0
Haidan1011 <101380985+Haidan1011@users.noreply.github.com>
2022-09-28 23:33:29 +0700

    add property for GraphicsComponent

--- src/main/java/com/myproject/bomberman/GraphicsComponent.java
+++ src/main/java/com/myproject/bomberman/GraphicsComponent.java
@@ -1,4 +1,12 @@
 package com.myproject.bomberman;
 
+import com.almasb.fxgl.texture.AnimatedTexture;
+import com.almasb.fxgl.texture.AnimationChannel;
+import javafx.scene.image.Image;
+
 public class GraphicsComponent {
+    private Image mainImage;
+    private String type;
+    private AnimationChannel anim;
+    private AnimatedTexture texture;
 }
